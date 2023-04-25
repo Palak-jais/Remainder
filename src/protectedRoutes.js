ae0dba95c1eb92a7dca5611434487e750092439a
palak jaiswal <118873142+Palak-jais@users.noreply.github.com>
2023-04-25 17:51:00 +0530

    Update protectedRoutes.js
    *

--- src/protectedRoutes.js
+++ src/protectedRoutes.js
@@ -4,6 +4,6 @@ import Login from './login';
 
 const ProtectedRoutes=()=>{
     const loggedin=window.localStorage.getItem("loggedIn")
-       return loggedin?<Outlet/>:<Login/>
+       return loggedin?<Outlet/>:<div className='warn'><h1>Please Login First!😅</h1></div>
 }
 export default ProtectedRoutes;
