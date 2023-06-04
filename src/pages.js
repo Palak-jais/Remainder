eb11db9d4dbbcf02cf5b2fd6b3549c27a99399f4
palak jaiswal <118873142+Palak-jais@users.noreply.github.com>
2023-06-04 12:42:10 +0530

    Update pages.js

--- src/pages.js
+++ src/pages.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from  'react-router-dom';
-import Login from './login'
-import Register from './register';
 import App from './App';
-import Home from './Home'
 import Nav from './Navbar'
-import Logout from './logout';
-import ProtectedRoutes from './protectedRoutes';
 
 
 function Pages(){
@@ -14,19 +9,10 @@ function Pages(){
     return(
         <>
         
-        <Router>
-        
+        <Router>        
         <Routes>
         <Route path="/" element={<Nav/>}>
-        <Route path="/" index element={<Home/>} />
-        <Route path="login" element={<Login/>} />
-        <Route path="register" element={<Register/>} />
-        <Route path="logout" element={<Logout/>} />
-         <Route element={<ProtectedRoutes/>}>
-        <Route path="todo" element={<App/>} />
-        </Route>
-        </Route>
-        
+        <Route path="/" index element={<App/>}        
         </Routes>
         </Router>
         
